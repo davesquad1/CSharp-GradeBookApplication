09e1e6d57cfb47bb125ea3f22c292b6ec238a623
Dave Buck <dbuckdev@DEV1.ajbell.com>
2019-05-09 15:42:54 +0100

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,29 +12,30 @@ namespace GradeBook.GradeBooks
         
         public override char GetLetterGrade(double averageGrade)
         {
-            if (Students.Count < 5)
-            {
-                throw new System.InvalidOperationException("F");
-            }
-
-       
-            if (averageGrade >= (80 / 100 * Students.Count))
-            {
-                return 'A';
-            }
-            else if (averageGrade >= (60 / 100 * Students.Count) && averageGrade < (80 / 100 * Students.Count))
-            {
-                return 'B';
-            }
-            else if (averageGrade >= (40 / 100 * Students.Count) && averageGrade < (60 / 100 * Students.Count))
-            {
-                return 'C';
-            }
-            else if (averageGrade <= (20 / 100 * Students.Count))
-            {
-                return 'D';
-            }
             return GetLetterGrade(averageGrade);
+            //if (Students.Count < 5)
+            //{
+            //    throw new System.InvalidOperationException("F");
+            //}
+
+
+            //if (averageGrade >= (80 / 100 * Students.Count))
+            //{
+            //    return 'A';
+            //}
+            //else if (averageGrade >= (60 / 100 * Students.Count) && averageGrade < (80 / 100 * Students.Count))
+            //{
+            //    return 'B';
+            //}
+            //else if (averageGrade >= (40 / 100 * Students.Count) && averageGrade < (60 / 100 * Students.Count))
+            //{
+            //    return 'C';
+            //}
+            //else if (averageGrade <= (20 / 100 * Students.Count))
+            //{
+            //    return 'D';
+            //}
+            //return GetLetterGrade(averageGrade);
         }     
     }
 }
