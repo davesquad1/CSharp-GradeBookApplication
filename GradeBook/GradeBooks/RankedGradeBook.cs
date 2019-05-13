da4a70745cb4558b10d4c5b937d4768f076b7a53
Dave Buck <dbuckdev@DEV1.ajbell.com>
2019-05-13 09:42:57 +0100

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -18,23 +18,23 @@ namespace GradeBook.GradeBooks
             }
             return GetLetterGrade(averageGrade);
 
-            //if (averageGrade >= (80 / 100 * Students.Count))
-            //{
-            //    return 'A';
-            //}
-            //else if (averageGrade >= (60 / 100 * Students.Count) && averageGrade < (80 / 100 * Students.Count))
-            //{
-            //    return 'B';
-            //}
-            //else if (averageGrade >= (40 / 100 * Students.Count) && averageGrade < (60 / 100 * Students.Count))
-            //{
-            //    return 'C';
-            //}
-            //else if (averageGrade <= (20 / 100 * Students.Count))
-            //{
-            //    return 'D';
-            //}
-            //return GetLetterGrade(averageGrade);
+            if (averageGrade >= (80 / 100 * Students.Count))
+            {
+                return 'A';
+            }
+            else if (averageGrade >= (60 / 100 * Students.Count) && averageGrade < (80 / 100 * Students.Count))
+            {
+                return 'B';
+            }
+            else if (averageGrade >= (40 / 100 * Students.Count) && averageGrade < (60 / 100 * Students.Count))
+            {
+                return 'C';
+            }
+            else if (averageGrade <= (20 / 100 * Students.Count))
+            {
+                return 'D';
+            }
+            return GetLetterGrade(averageGrade);
         }
     }
 }
