707eb934b61bf2ceeb9ec05b6b943fa620be5c1b
Dave Buck <dbuckdev@DEV1.ajbell.com>
2019-05-14 10:29:27 +0100

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -30,6 +30,20 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count <= 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
         public override void CalculateStudentStatistics(string name)
         {
             if (Students.Count < 5)
@@ -37,8 +51,6 @@ namespace GradeBook.GradeBooks
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
             }
-
-
             else
             {
                 base.CalculateStudentStatistics(name);
