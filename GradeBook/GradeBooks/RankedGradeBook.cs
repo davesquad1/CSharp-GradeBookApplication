f7a0075de34bde85b60d2948607b74ac3d641be0
Dave Buck <dbuckdev@DEV1.ajbell.com>
2019-05-14 08:17:26 +0100

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,5 @@
 ﻿using GradeBook.Enums;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace GradeBook.GradeBooks
@@ -30,71 +29,8 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
-
-            //return base.GetLetterGrade(averageGrade);
-
-            //var sum = 0;
-            //sum = (int)grades.Sum();
-
-            //if (sum <= threshold)
-            //    return 'A';
-            //else if (sum <= (threshold * 2) && sum > threshold)
-            //    return 'B';
-            //else if (sum <= (threshold * 6) && sum > (threshold * 4))
-            //    return 'C';
-            //else if (sum <= (threshold * 8) && sum > (threshold * 6))
-            //    return 'D';
-            //else
-            //    return 'F';
-
-            // return base.GetLetterGrade(averageGrade);
-
-            //if (averageGrade >= threshold * Students.Count)
-            //{
-            //    return 'A';
-            //}
-            //else if (averageGrade >= (threshold * 4) * Students.Count && averageGrade < (threshold * 2) * Students.Count)
-            //{
-            //    return 'B';
-            //}
-            //else if (averageGrade >= (threshold  * 6) * Students.Count && averageGrade < (threshold * 4) * Students.Count)
-            //{
-            //    return 'C';
-            //}
-            //else if (averageGrade >= (threshold * 8) * Students.Count && averageGrade < (threshold * 6) * Students.Count)
-            //{
-            //    return 'D';
-            //}
-            //else
-            //{
-            //  // 
-            //    return 'F';
-            //}
-
-
-        }
-
-            
-            
-
-            //if (averageGrade >= (80 / 100 * Students.Count))
-            //{
-            //    return 'A';
-            //}
-            //else if (averageGrade >= (60 / 100 * Students.Count) && averageGrade < (80 / 100 * Students.Count))
-            //{
-            //    return 'B';
-            //}
-            //else if (averageGrade >= (40 / 100 * Students.Count) && averageGrade < (60 / 100 * Students.Count))
-            //{
-            //    return 'C';
-            //}
-            //else if (averageGrade <= (20 / 100 * Students.Count))
-            //{
-            //    return 'D';
-            //}
-           // return GetLetterGrade(averageGrade);
-        }
+        }   
     }
+ }
 
 
